c185491bc917332b3e34eead4caa3271134c6794
Patrick King <pat.k195@gmail.com>
2024-01-28 18:52:34 -0500

    Longest_Word

---
+++ Longest_Word.py
@@ -0,0 +1,43 @@
+import string
+
+
+#function that finds the longest word
+def LongestWord(sen):
+
+  # splits the string into a list
+  long = sen.split(" ")
+
+
+  #for loop that uses the length of the list to go through each word  
+  for i in range(len(long)):
+
+    # Make a translation table that maps all punctuation characters to None
+    translator = str.maketrans("", "", string.punctuation)
+
+    # Apply the translation table to each word in the list
+    long[i] = long[i].translate(translator)
+    
+    #if its not the first word
+    if i != 0:
+      #get the length of the current word in the loop
+      length = len(long[i])
+      #if that length is greater than the current longest word (sen)
+      if length > len(sen):
+        #make sen the new word
+        sen = long[i]
+      else:
+        #do nothing if its not longer
+        continue
+
+
+    #first word is set to the longest word
+    elif i == 0:
+      sen = long[i]
+  
+
+
+
+  return sen
+
+#calls LongestWord function with user input
+print(LongestWord(input()))
